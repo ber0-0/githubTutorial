32ffeb4e521f8825a0afc74d757631cce0b44ff4
ber0.0 <bernicejingliang@gmail.com>
2020-02-10 11:35:36 -0800

    fix bugs

--- myScript.py
+++ myScript.py
@@ -5,10 +5,10 @@ def my_func(matrix_1, matrix_2):
     try:
         return matrix_1 * matrix_2
     except ValueError:
-        return none
+        return None
 
 my_matrix = np.identity(4)
-second_matrix = np.random.random(size(4,4))
+second_matrix = np.random.random(size=(4,4))
 
 print(my_func(my_matrix, second_matrix))
 
