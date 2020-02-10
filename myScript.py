d6184ea8bc1c3206a8e36cb43b93baad61f1379d
ber0.0 <bernicejingliang@gmail.com>
2020-02-10 11:23:34 -0800

    added a function which returns the product matrix

--- myScript.py
+++ myScript.py
@@ -1,11 +1,19 @@
 import numpy as np
 import pandas as pd
 
+def my_func(matrix_1, matrix_2):
+    return matrix_1 * matrix_2
 
-n = input("Enter an integer:")
+my_matrix = np.identity(4)
+second_matrix = np.identity(4)
 
-for i in range(n):
-    print(n)
+print(my_func(my_matrix, second_matrix))
 
-my_matrix = np.identity(n)
-print(my_matrix)
+
+#n = input("Enter an integer:")
+
+#for i in range(n):
+#    print(n)
+
+
+#print(my_matrix)
