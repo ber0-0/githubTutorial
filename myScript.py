a185e7a8f1c4ed8a763baf8865b7144634f9e1c2
ber0.0 <bernicejingliang@gmail.com>
2020-02-10 11:26:49 -0800

    fixed the function by using a try/except block

--- myScript.py
+++ myScript.py
@@ -2,10 +2,13 @@ import numpy as np
 import pandas as pd
 
 def my_func(matrix_1, matrix_2):
-    return matrix_1 * matrix_2
+    try:
+        return matrix_1 * matrix_2
+    except ValueError:
+        return none
 
 my_matrix = np.identity(4)
-second_matrix = np.identity(4)
+second_matrix = np.random.random(size(4,4))
 
 print(my_func(my_matrix, second_matrix))
 
