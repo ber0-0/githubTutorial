37103cfc16b132187b1effd9f50132e6f78fc925
ber0.0 <bernicejingliang@gmail.com>
2020-02-10 11:32:41 -0800

    change sizes of matrix

--- myScript.py
+++ myScript.py
@@ -2,10 +2,12 @@ import numpy as np
 import pandas as pd
 
 
-n = input("Enter an integer:")
+#n = input("Enter an integer:")
 
-for i in range(n):
-    print(n)
+#for i in range(n):
+#    print(n)
 
-my_matrix = np.identity(n)
-print(my_matrix)
+my_matrix = np.identity(5)
+second_matrix = np.random.random(size(5,4))
+
+print(my_matrix, second_matrix)
